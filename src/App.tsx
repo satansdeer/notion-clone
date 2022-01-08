237a823cb0974615ad3647cd6d3167ed89ac254d
Maksim Ivanov <satansdeer@gmail.com>
2022-01-08 21:30:36 +0100

    Add autofocus

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const initialState = [
   {
     type: "paragraph",
     value: "Test paragraph",
+		id: Date.now()
   },
 ];
 
-const renderNode = ({ type, value }: any, onKeyPress: any) => {
+const renderNode = ({ type, value }: any, onKeyPress: any, refFunc: any) => {
   switch (type) {
     case "paragraph": {
       return (
-        <p onKeyPress={onKeyPress} contentEditable="true">
+        <p ref={refFunc} onKeyPress={onKeyPress} contentEditable="true">
           {value}
         </p>
       );
@@ -21,17 +22,29 @@ const renderNode = ({ type, value }: any, onKeyPress: any) => {
 
 function App() {
   const [nodes, setNodes] = useState(initialState);
+	const nodeIdToFocusRef = useRef<any>(null)
+	const nodesRef = useRef<any>({})
 
 	const onKeyPress = (event: any) => {
 		if(event.key === "Enter"){
-			setNodes(oldNodes => [...oldNodes, {type: "paragraph", value: ""}])
+			const id = Date.now()
+			nodeIdToFocusRef.current = id;
+			setNodes(oldNodes => [...oldNodes, {type: "paragraph", value: "", id}])
+		}
+	}
+
+	const onRef = (nodeId: any) => (el: any) => {
+		nodesRef.current[nodeId] = el
+		if(nodeId === nodeIdToFocusRef.current) {
+			el.focus()
+			nodeIdToFocusRef.current = null;
 		}
 	}
 
   return (
     <>
       {nodes.map((node) => {
-        return renderNode(node, onKeyPress);
+        return renderNode(node, onKeyPress, onRef(node.id));
       })}
     </>
   );
