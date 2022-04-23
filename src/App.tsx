c0629a645377adc37ff41a4b64191beb0c78bbbb
Maksim Ivanov <satansdeer@gmail.com>
2022-04-23 13:37:38 +0200

    Add supabase + auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Page } from "./Page";
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+import Auth from "./Auth";
 
 function App() {
-  return (
+  const [session, setSession] = useState<any>(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event: any, session: any) => {
+      setSession(session);
+    });
+  }, []);
+
+  return !session ? (
+    <Auth />
+  ) : (
     <Routes>
       <Route path="/:id" element={<Page />} />
       <Route path="/" element={<Navigate to="/start" />} />
